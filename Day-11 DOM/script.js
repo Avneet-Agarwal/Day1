7403d037c99b05b8b8280d64cb7a4fdf3b459ea3
Avneet-Agarwal <funnymagnet3005@gmail.com>
2022-08-23 12:15:23 +0530

    commit

--- Day-11 DOM/script.js
+++ Day-11 DOM/script.js
@@ -6,13 +6,13 @@ var startmole = document.querySelector("#mole");
 var btn = document.querySelector("#start");
 var points = document.querySelectorAll(".points");
 var moles = document.querySelectorAll(".mole");
-
 var counter = 0;
 function start() {
     h1.style.visibility = "hidden";
     startmud.style.visibility = "hidden";
     btn.style.visibility = "hidden";
     startmole.style.visibility = "hidden";
+    
     for (let i = 0; i < 6; i++) {
         dirt[i].style.visibility = "visible";
     }
@@ -25,8 +25,8 @@ function start() {
         moles[ran].style.visibility = "visible";
         setTimeout(() => {
             moles[ran].style.visibility = "hidden";
-        }, 900);
-    }, 1000);
+        }, 1400);
+    }, 1500);
 
 }
 function clicked() {
