63a80669a365efd74a7cffcb20ff2cddaceeb615
Avneet-Agarwal <funnymagnet3005@gmail.com>
2022-08-24 18:31:12 +0530

    form submit

---
+++ Day-13 loader website/script.js
@@ -0,0 +1,7 @@
+window.onload=setTimeout(hideobj, 3000);
+function hideobj(){
+    var num1= document.getElementsByClassName('container1');
+    num1[0].style.display="none";
+    var num2=document.getElementsByClassName("container2");
+    num2[0].style.display="block";
+}
