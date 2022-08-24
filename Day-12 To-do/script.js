1211ee6e7e6fc014496f6b5c4a0e71920e5ed799
Avneet-Agarwal <funnymagnet3005@gmail.com>
2022-08-24 10:59:01 +0530

    commit

---
+++ Day-12 To-do/script.js
@@ -0,0 +1,8 @@
+// selectors
+
+var inp=document.querySelector('.todo-input');
+var btn=document.querySelector('button');
+var list=document.querySelector('.todo-list');
+btn.onclick=function crtodo(e){
+    e.prevent
+}
