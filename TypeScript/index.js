107b06c813d81c299905bcd61ef9769c76411ed8
Avneet-Agarwal <funnymagnet3005@gmail.com>
2022-09-13 18:27:46 +0530

    commit

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -2,7 +2,7 @@ console.log("hello world");
 var t1 = document.getElementById("val1");
 var t2 = document.getElementById("val2");
 var t3 = document.getElementById("res");
-var submit = document.getElementById("submit");
+var submit = document.getElementById("add");
 var clicked = function () {
     var name1 = Number(t1.value);
     var name2 = Number(t2.value);
@@ -13,4 +13,4 @@ var clicked = function () {
     console.log("The result is ".concat(t3.value));
 };
 //submit.addEventListener('click',clicked);
-submit.addEventListener('keyup', clicked);
+submit.addEventListener('clicked', clicked);
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -2,7 +2,7 @@ console.log("hello world");
 var t1= document.getElementById("val1") as HTMLInputElement;
 var t2= document.getElementById("val2") as HTMLInputElement;
 var t3= document.getElementById("res") as HTMLInputElement;
-var submit= document.getElementById("submit") as HTMLButtonElement;
+var submit= document.getElementById("add") as HTMLButtonElement;
 
 var clicked=()=>{
 
@@ -16,6 +16,6 @@ console.log(`The result is ${t3.value}`);
 };
 //submit.addEventListener('click',clicked);
 
-submit.addEventListener('keyup',clicked);
+submit.addEventListener('clicked',clicked);
 
 
